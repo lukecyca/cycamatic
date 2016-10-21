b6e33a94a4d03120e88caacc27cd12c4786e67dc
Luke Cyca <me@lukecyca.com>
2016-10-21 11:28:28 -0700

    Switched to pynstagram and a config file

--- uploader.py
+++ uploader.py
@@ -1,52 +1,44 @@
 import time
 import os
-from instagram import InstagramSession
-
-USERNAME = "cycamatic"
-PASSWORD = "<password>"
-
-PHOTO_DIR = "/opt/photos/"
-PROCESSED_DIR = "/opt/photos-processed/"
+import os.path
+import configparser
+import pynstagram
 
 
-def upload_photo(filepath):
+def upload_photo(filepath, c):
     print "Converting {}".format(filepath)
     status = os.system(
-        "convert -sample 1280x1280^ -gravity Center "
+        "convert -auto-orient -sample 1280x1280^ -gravity Center "
         "-extent 1280x1280 {} /tmp/small.jpg".format(filepath)
     )
     if status & 0xff00:
         raise ValueError("Failed to convert")
 
     print "Uploading {}".format(filepath)
-    insta = InstagramSession()
-    if insta.login(USERNAME, PASSWORD):
-        media_id = insta.upload_photo("/tmp/small.jpg")
-        print media_id
-        if media_id is not None:
-            insta.configure_photo(media_id, "")
-            #try:
-            #    insta.permalink(media_id)
-            #except:
-            #    pass
-            return True
-
-    return False
+    with pynstagram.client(c['username'], c['password']) as client:
+        client.upload("/tmp/small.jpg", c['caption'])
 
 
 def main():
-    if not os.access(PHOTO_DIR, os.R_OK | os.W_OK | os.X_OK):
-        raise ValueError("Need permission to {}".format(PHOTO_DIR))
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    c = config['instagram']
+
+    source_dir = os.path.abspath(c['source_dir'])
+    dest_dir = os.path.abspath(c['dest_dir'])
+    throttle = int(c['throttle'])
+
+    if not os.access(source_dir, os.R_OK | os.W_OK | os.X_OK):
+        raise ValueError("Need permission to {}".format(source_dir))
 
     while True:
         time.sleep(1)
-        for fn in os.listdir(PHOTO_DIR):
-            path = os.path.join(PHOTO_DIR, fn)
-            if upload_photo(path):
-                print "Moving {}".format(path)
-                os.rename(path, os.path.join(PROCESSED_DIR, fn))
-            else:
-                print "Failed"
+        for fn in os.listdir(source_dir):
+            path = os.path.join(source_dir, fn)
+            upload_photo(path, c)
+            print "Moving {}".format(path)
+            os.rename(path, os.path.join(dest_dir, fn))
+            time.sleep(throttle)
 
 
 if __name__ == "__main__":
